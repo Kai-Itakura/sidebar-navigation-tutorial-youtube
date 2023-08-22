d15586ed28446ebd54e944c6443ed02d773352b3
Kai-Itakura <itakai199969@gmail.com>
2023-08-22 20:50:58 +0900

    学習完了

---
+++ src/components/SidebarIcon.jsx
@@ -0,0 +1,12 @@
+import Icon from '../images/icon.jpg'
+
+const SidebarIcon = () => {
+  return (
+    <div className='SidebarIcon'>
+      <img src={Icon} alt='' />
+      <p>programing@gmail.com</p>
+    </div>
+  )
+}
+
+export default SidebarIcon
